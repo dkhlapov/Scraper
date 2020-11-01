5bddb7a6b8e75e8242c00b4cf33ba125639ebbc7
Dmitrii <khlapov99@gmail.com>
2020-11-01 23:00:31 +0100

    code cleanup, now working without pandas

--- scraper.py
+++ scraper.py
@@ -1,16 +1,10 @@
-from bs4 import BeautifulSoup
-import urllib.request
-import numpy as np
-import pandas as pd
 import time as tm
-import logging
+import urllib.request
+
 import pymongo as mongo
+from bs4 import BeautifulSoup
 
 client = mongo.MongoClient("mongodb://127.0.0.1:27017")
-df_final = pd.DataFrame()
-#FORMAT = 'Hash: %(hash)s Time: %(time)s BTC: %(btc)f USD: %(usd)f'
-#logging.basicConfig(filename="tophashes.log", level=logging.INFO, format=FORMAT, filemode='w')
-#logger = logging.getLogger()
 scraper_db = client['scraper']
 col_hashes = scraper_db['hashes']
 while True:
@@ -18,23 +12,18 @@ while True:
     soup = BeautifulSoup(link, 'html.parser')
     hashes_raw = soup.find_all("a", class_="sc-1r996ns-0 gzrtQD sc-1tbyx6t-1 kXxRxe iklhnl-0 boNhIO d53qjk-0 jmTmMY")
     info = soup.find_all("span", class_="sc-1ryi78w-0 gCzMgE sc-16b9dsl-1 kUAhZx u3ufsr-0 fGQJzg")
+    pos = 0
+    maxusd = 0
     hashes, time, btc, usd = ([] for i in range(4))
     for i in range(len(info)):
-        if (i % 3 == 0):
-            time.append(info[i].string)
-        if (i % 3 == 1):
-            btc.append(float(info[i].string[:-3].replace(",", "")))
-        if (i % 3 == 2):
-            usd.append(float(info[i].string[1:].replace(",", "")))
-    for row in hashes_raw:
-        hashes.append(row.string)
-    dict = {'hash':hashes, 'time': time, 'btc': btc, 'usd': usd}
-    df = pd.DataFrame(data=dict)
-    df = df.sort_values(by='usd', ascending=False)
-    records = df.head(1).to_dict('records')
-    d = records[0]
+        if ((i % 3 == 2) and (float(info[i].string[1:].replace(",", "")) > maxusd)):
+            pos = i
+            maxusd = float(info[i].string[1:].replace(",", ""))
+    tophash = hashes_raw[pos // 3].string
+    topusd = maxusd
+    toptime = info[pos - 2].string
+    topbtc = float(info[pos - 1].string[:-3].replace(",", ""))
+    print(tophash, topusd, toptime, topbtc)
+    d = {'hash': tophash, 'time': toptime, 'btc': topbtc, 'usd': topusd}
     insert_hash = col_hashes.insert_one(d)
-    #df_final = df_final.append(df.head(1))
-    #logger.info('', extra=d)
     tm.sleep(60)
-#print(df_final)
