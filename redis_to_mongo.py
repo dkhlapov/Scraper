7fe7a50b244ab690558623874c94d06b0c71660a
Dmitrii Khlapov <khlapov99@gmail.com>
2020-11-20 03:14:55 +0100

    redis_to_mongo.py finished and operational tophashes.log deleted because not needed anymore

--- redis_to_mongo.py
+++ redis_to_mongo.py
@@ -1,9 +1,24 @@
 import pymongo as mongo
 import redis
 from scraper_to_redis import redis_setup
+import time
 
 r = redis.Redis()
 client = mongo.MongoClient("mongodb://127.0.0.1:27017")
 scraper_db = client['scraper']
 col_hashes = scraper_db['hashes']
-redis_setup()
+
+def get_values(keys):
+    pipe = r.pipeline()
+    for key in keys:
+        pipe.hgetall(key)
+    values = pipe.execute()
+    values = [{key.decode('utf-8'):value.decode('utf-8') for key,value in d.items()} for d in values]
+    d = dict(zip(keys, values))
+    d = {key.decode('utf-8'):value for key, value in d.items()}
+    return d
+while True:
+    redis_setup()
+    keys = r.keys()
+    col_hashes.insert_one(dict(list(sorted(get_values(keys).items(), key=lambda x: float(x[1]["usd"]), reverse=True))[:1]))
+    time.sleep(60)
--- scraper_to_redis.py
+++ scraper_to_redis.py
@@ -13,11 +13,11 @@ def redis_setup():
     soup = BeautifulSoup(link, 'html.parser')
     hashes_raw = [a.string for a in
                   soup.find_all("a", class_="sc-1r996ns-0 gzrtQD sc-1tbyx6t-1 kXxRxe iklhnl-0 boNhIO d53qjk-0 jmTmMY")]
-    bitcoin = [a.string for a in soup.find_all("span", class_="sc-1ryi78w-0 gCzMgE sc-16b9dsl-1 kUAhZx u3ufsr-0 fGQJzg",
+    bitcoin = [a.string[:-4].replace(',','') for a in soup.find_all("span", class_="sc-1ryi78w-0 gCzMgE sc-16b9dsl-1 kUAhZx u3ufsr-0 fGQJzg",
                                                string=re.compile(r'\d+,*\d*\.\d+ BTC'))]
     time = [a.string for a in soup.find_all("span", class_="sc-1ryi78w-0 gCzMgE sc-16b9dsl-1 kUAhZx u3ufsr-0 fGQJzg",
                                             string=re.compile(r'\d\d:\d\d'))]
-    dollars = [a.string for a in soup.find_all("span", class_="sc-1ryi78w-0 gCzMgE sc-16b9dsl-1 kUAhZx u3ufsr-0 fGQJzg",
+    dollars = [a.string[1:].replace(',', '') for a in soup.find_all("span", class_="sc-1ryi78w-0 gCzMgE sc-16b9dsl-1 kUAhZx u3ufsr-0 fGQJzg",
                                                string=re.compile(r'\$\d+,*\d*\.\d+'))]
     d = {}
     for (item_hashes, item_time, item_btc, item_usd) in zip(hashes_raw, time, bitcoin, dollars):
