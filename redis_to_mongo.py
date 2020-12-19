92712af022465553f3e2236431bd89176b852739
Dmitrii Khlapov <khlapov99@gmail.com>
2020-12-19 22:39:14 +0100

    Update redis_to_mongo.py

--- redis_to_mongo.py
+++ redis_to_mongo.py
@@ -3,8 +3,8 @@ import redis
 from scraper_to_redis import redis_setup
 import time
 
-r = redis.Redis(host='172.17.0.2', port=6379)
-client = mongo.MongoClient("mongodb://172.17.0.3")
+r = redis.Redis(host='172.17.0.1', port=6379)
+client = mongo.MongoClient("mongodb://172.17.0.2")
 scraper_db = client['scraper']
 col_hashes = scraper_db['hashes']
 
